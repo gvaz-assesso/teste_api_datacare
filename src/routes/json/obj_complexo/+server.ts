2edf9fc8604aaebf6d4ea6e1852224f20630a16f
Giovani Sant'Ana Ferreira Pedroso Vaz <giovanisfpv@gmail.com>
2025-10-02 11:23:44 -0300

    a

--- src/routes/json/obj_complexo/+server.ts
+++ src/routes/json/obj_complexo/+server.ts
@@ -1,42 +1,39 @@
 import { json } from '@sveltejs/kit';
 
-type MeuObjeto = {
-	msg: string;
-	id: number;
-	item: {
-		nome: string;
-		preco: number;
-		cat: string[];
-	};
+type Post = {
+	id: string;
+	titulo: string;
+	date: string;
+	coments: {
+		id: string;
+		autor: string;
+		text: string;
+		date: string;
+		id_post: string;
+	}[];
 };
 
-const dados: MeuObjeto[] = [
+const dados: Post[] = [
 	{
-		msg: 'Objeto 1',
-		id: 1,
-		item: {
-			nome: 'Teclado Mecânico',
-			preco: 199.9,
-			cat: ['periférico', 'gaming']
-		}
-	},
-	{
-		msg: 'Objeto 2',
-		id: 2,
-		item: {
-			nome: 'Mouse Gamer',
-			preco: 89.9,
-			cat: ['periférico', 'gaming']
-		}
-	},
-	{
-		msg: 'Objeto 3',
-		id: 3,
-		item: {
-			nome: 'Headset USB',
-			preco: 129.5,
-			cat: ['periférico', 'audio']
-		}
+		id: '1',
+		titulo: 'olar',
+		date: '01-12-2023',
+		coments: [
+			{
+				id: '1',
+				autor: 'carlos',
+				text: 'muito bom',
+				date: '01-12-2024',
+				id_post: '1'
+			},
+			{
+				id: '2',
+				autor: 'maria',
+				text: 'muito bom',
+				date: '01-12-2024',
+				id_post: '1'
+			}
+		]
 	}
 ];
 
@@ -45,38 +42,38 @@ export const GET = async () => {
 };
 
 // Função de checagem
-function isMeuObjeto(obj: any): obj is MeuObjeto {
-	return (
-		typeof obj === 'object' &&
-		obj !== null &&
-		typeof obj.msg === 'string' &&
-		typeof obj.id === 'number' &&
-		typeof obj.item === 'object' &&
-		obj.item !== null &&
-		typeof obj.item.nome === 'string' &&
-		typeof obj.item.preco === 'number' &&
-		Array.isArray(obj.item.cat) &&
-		obj.item.cat.every((c: any) => typeof c === 'string')
-	);
-}
+// function isMeuObjeto(obj: any): obj is Post {
+// 	return (
+// 		typeof obj === 'object' &&
+// 		obj !== null &&
+// 		typeof obj.msg === 'string' &&
+// 		typeof obj.id === 'number' &&
+// 		typeof obj.item === 'object' &&
+// 		obj.item !== null &&
+// 		typeof obj.item.nome === 'string' &&
+// 		typeof obj.item.preco === 'number' &&
+// 		Array.isArray(obj.item.cat) &&
+// 		obj.item.cat.every((c: any) => typeof c === 'string')
+// 	);
+// }
 
 // Checagem para array de objetos
-function isMeuObjetoArray(arr: any): arr is MeuObjeto[] {
-	return Array.isArray(arr) && arr.every(isMeuObjeto);
-}
+// function isMeuObjetoArray(arr: any): arr is MeuObjeto[] {
+// 	return Array.isArray(arr) && arr.every(isMeuObjeto);
+// }
 
 export async function POST({ request }) {
 	const body = await request.json();
 	console.log(body);
 
 	if (Array.isArray(body)) {
-		if (isMeuObjetoArray(body)) {
-			return json({ msg: 'array postado corretamente' }, { status: 201 });
-		}
+		// if (isMeuObjetoArray(body)) {
+		return json({ msg: 'array postado corretamente' }, { status: 201 });
+		// }
 	} else {
-		if (isMeuObjeto(body)) {
-			return json({ msg: 'objeto postado corretamente' }, { status: 201 });
-		}
+		// if (isMeuObjeto(body)) {
+		return json({ msg: 'objeto postado corretamente' }, { status: 201 });
+		// }
 	}
 
 	return json({ msg: 'post com erros' }, { status: 403 });
